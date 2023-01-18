ee9ce2283cc82f40f7d259b76f97738b6096dcd0
Matheus Alves <matalves12345@gmail.com>
2023-01-18 17:16:19 -0300

    base project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to blackjack-clojure
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/blackjack_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns blackjack-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [blackjack-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
